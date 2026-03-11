6010ecb86badd38877b3f52d37385156f0307145
Sahib Bains <117793342+PutridThunder@users.noreply.github.com>
2026-03-10 20:24:06 -0700

    Refactor grade processing and visualization in Crowdmark.py
    Refactored code to read class information and grades more efficiently. Updated plotting logic to visualize grades with improved handling of missing values.

--- Crowdmark-Type/Crowdmark.py
+++ Crowdmark-Type/Crowdmark.py
@@ -3,24 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with open("Crowdmark.csv", "r") as f:
+    Titles = f.readline().strip("\n").split()
+    ClassInfo = [studentline.strip().split() for studentline in f]
 
-    Titles = f.readline().strip("\n").split(" ")
-
-    ClassInfo = []
-
-    for studentline in f.readlines():
-        ClassInfo.append(studentline.strip("\n").split(" "))
-
-Dict = {"FirstNames":[x[0] for x in ClassInfo], "LastNames":[x[1] for x in ClassInfo], "Grades":[x[2:] for x in ClassInfo]}
+Dict = {
+    "FirstNames":[x[0] for x in ClassInfo],
+    "LastNames":[x[1] for x in ClassInfo],
+    "Grades":[x[2:] for x in ClassInfo]
+    }
 
 Dataframe = pd.DataFrame(Dict)
-Grades = Dataframe.get("Grades")
+NumOfAssignments = max(len(x) for x in Dict["Grades"])
+GradesMatrix = np.array([x + [np.nan] * (NumOfAssignments - len(x)) for x in Dict["Grades"]])
 
-for i in range(len(Grades)):
-    x = np.array([x+1 for x in range(len(Grades[i]))])
-    y = [int(g) for g in Grades[i]]
-
-    plt.plot(x, y, marker = "o", label=f"{Dataframe.get('FirstNames')[i]}")
-
-plt.legend(loc="upper left")
+plt.ylim(0,100)
+for studentgrades in GradesMatrix:
+    plt.plot(
+        [x+1 for x in range(NumOfAssignments)], studentgrades, marker= 'o', color = "grey", alpha = 0.2)
 plt.show()
